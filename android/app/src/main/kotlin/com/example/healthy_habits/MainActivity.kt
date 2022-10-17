341d1c4582b76c31cc6f5d1fd48e22bad970db36
dmerry15 <danalakers2@gmail.com>
2022-10-17 14:45:17 -0400

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/healthy_habits/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healthy_habits
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+const COLOR_MINT = Color.fromRGBO(176, 255, 204, 1.0);
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Healthy Habits',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: COLOR_MINT,
+          title: Text(
+            'Healthy Habits',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Column(
+          children: [
+            Text(
+              "Welcome! Please select an option from the menu!",
+              style: TextStyle(fontSize: 28),
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              onPressed: () => print("Survey Loading..."),
+              child: Text('Take Survey'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(COLOR_MINT),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => print("Item 2 Loading..."),
+              child: Text('Item 2'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(COLOR_MINT),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => print("Item 3 Loading..."),
+              child: Text('Item 3'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(COLOR_MINT),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
