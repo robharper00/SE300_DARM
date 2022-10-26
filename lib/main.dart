294323fb134f286f62023d88eeadcf5862a5acd7
dmerry15 <danalakers2@gmail.com>
2022-10-26 16:43:15 -0400

    Updating the main file, revamping GUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-const COLOR_MINT = Color.fromRGBO(176, 255, 204, 1.0);
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -15,48 +14,55 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Healthy Habits',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: COLOR_MINT,
-          title: Text(
-            'Healthy Habits',
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: Column(
-          children: [
-            Text(
-              "Welcome! Please select an option from the menu!",
-              style: TextStyle(fontSize: 28),
-              textAlign: TextAlign.center,
-            ),
-            ElevatedButton(
-              onPressed: () => print("Survey Loading..."),
-              child: Text('Take Survey'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(COLOR_MINT),
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () => print("Item 2 Loading..."),
-              child: Text('Item 2'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(COLOR_MINT),
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () => print("Item 3 Loading..."),
-              child: Text('Item 3'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(COLOR_MINT),
-                foregroundColor: MaterialStateProperty.all(Colors.black),
-              ),
-            ),
-          ],
-        ),
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        accentColor: Colors.white,
+        canvasColor: Colors.black,
       ),
     );
   }
 }
+      // title: 'Healthy Habits',
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     backgroundColor: COLOR_MINT,
+      //     title: Text(
+      //       'Healthy Habits',
+      //       style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+      //     ),
+      //   ),
+      //   body: Column(
+      //     children: [
+      //       Text(
+      //         "Welcome! Please select an option from the menu!",
+      //         style: TextStyle(fontSize: 28),
+      //         textAlign: TextAlign.center,
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: () => print("Survey Loading..."),
+      //         child: Text('Take Survey'),
+      //         style: ButtonStyle(
+      //           backgroundColor: MaterialStateProperty.all(COLOR_MINT),
+      //           foregroundColor: MaterialStateProperty.all(Colors.black),
+      //         ),
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: () => print("Item 2 Loading..."),
+      //         child: Text('Item 2'),
+      //         style: ButtonStyle(
+      //           backgroundColor: MaterialStateProperty.all(COLOR_MINT),
+      //           foregroundColor: MaterialStateProperty.all(Colors.black),
+      //         ),
+      //       ),
+      //       ElevatedButton(
+      //         onPressed: () => print("Item 3 Loading..."),
+      //         child: Text('Item 3'),
+      //         style: ButtonStyle(
+      //           backgroundColor: MaterialStateProperty.all(COLOR_MINT),
+      //           foregroundColor: MaterialStateProperty.all(Colors.black),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+
