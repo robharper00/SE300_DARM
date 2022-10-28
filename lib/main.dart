46812b06dd73d5997887338a8b04f86aaa2d401d
dmerry15 <danalakers2@gmail.com>
2022-10-28 12:25:52 -0400

    Continued UI design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+const TITLE = 'Healthy Habits';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -13,11 +14,74 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Healthy Habits',
+      title: TITLE,
       theme: ThemeData(
-        primaryColor: Colors.green,
+        primarySwatch: Colors.green,
         accentColor: Colors.white,
         canvasColor: Colors.black,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text(TITLE),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Text(
+                  'Welcome to Healthy Habits! Please select an option below.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                height: 200,
+                width: double.infinity,
+                color: Color.fromARGB(255, 40, 37, 36),
+                child: Column(
+                  children: [
+                    Text(
+                      'Pedometer: Track Your Steps Here!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                        ),
+                        CircleAvatar(
+                          radius: 75,
+                          child: Text(
+                            '95000',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 48,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
